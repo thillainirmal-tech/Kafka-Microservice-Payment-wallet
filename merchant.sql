987d69e182fec52d7ca7c852827a9e0ee2358ac4
Thillai Nirmal K <120569119+thillainirmal-tech@users.noreply.github.com>
2025-09-30 18:01:00 +0530

    Add files via upload

---
+++ merchant.sql
@@ -0,0 +1,2 @@
+id,created_at,email,merchant_key,name,redirection_url,status_webhook,updated_at
+1,"2025-09-29 20:37:23.000000",shanmugakannan7549@gmail.com,demo-key-123,"Demo Merchant",http://localhost/payment/result?txnId=,http://localhost/payment/webhook,"2025-09-29 22:24:03.000000"
